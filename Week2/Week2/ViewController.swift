298565ead14eda3bc18eb348b99f6c213749c6eb
yeonsu <drawingbonny@gmail.com>
2022-09-26 22:43:58 +0900

    iOS 스터디 1주 차 세미나 실습 프로젝트 - Auto layout

---
+++ Week2/Week2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Week2
+//
+//  Created by yeonsu on 2022/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var newItemView: UIView!
+    
+    @IBAction func profileButtonDidtapped(_ sender: Any) {
+        print("버튼이 클릭됐습니다!")
+        newItemView.backgroundColor = UIColor.lightGray
+    }
+    
+       }
+
