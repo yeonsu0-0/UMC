0ea22875324aa47213909eba0092381b1b7a5f67
yeonsu <drawingbonny@gmail.com>
2022-09-30 23:59:26 +0900

    Add main objects

---
+++ Weekly-project/Week2-project/Week2-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2-project
+//
+//  Created by yeonsu on 2022/09/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
