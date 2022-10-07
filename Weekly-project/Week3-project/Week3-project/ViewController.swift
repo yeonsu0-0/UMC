6cdddede7f92bd3fa3be8179a62b2ef235064355
yeonsu <drawingbonny@gmail.com>
2022-10-07 23:55:16 +0900

    feat: Add present view controller

--- Weekly-project/Week3-project/Week3-project/ViewController.swift
+++ Weekly-project/Week3-project/Week3-project/ViewController.swift
@@ -18,13 +18,81 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
     }
+    
+    
+    // =============== 나머지 연산자 버튼 ===============
+    @IBAction func modButtonDidTapped(_ sender: Any) {
 
-// =============== 덧셈 연산자 버튼 ===============
-
+        guard let modViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "modViewController") as? modViewController else {return}
+        
+        guard let firstNum = firstTextfield.text else {return}
+        
+        
+        guard let secondNum = secondTextfield.text else {return}
+        
+        // String 타입으로 값이 넘어오기 때문에 형변환 필요
+        // But, Int값이 들어오지 않으면 에러 발생! (신중히 사용)
+        let result = Int(firstNum)! % Int(secondNum)!
+        
+        modViewController.resultString = String(result)
+        
+        modViewController.modalPresentationStyle = .fullScreen
+        
+        present(modViewController, animated: true)
+    
+    }
+    
+    // =============== 나눗셈 연산자 버튼 ===============
+    @IBAction func divideButtonDidTapped(_ sender: Any) {
+        guard let divideViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "divideViewController") as? divideViewController else {return}
+      
+        guard let firstNum = firstTextfield.text else {return}
+        
+        
+        guard let secondNum = secondTextfield.text else {return}
+        
+        // String 타입으로 값이 넘어오기 때문에 형변환 필요
+        // But, Int값이 들어오지 않으면 에러 발생! (신중히 사용)
+        let result = Int(firstNum)! / Int(secondNum)!
+        
+        divideViewController.resultString = String(result)
+        
+        divideViewController.modalPresentationStyle = .fullScreen
+        
+        present(divideViewController, animated: true)
+        
+    }
+    
+    
+    // =============== 뺄셈 연산자 버튼 ===============
+    @IBAction func minusButtonDidTapped(_ sender: Any) {
+        guard let minusViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "minusViewController") as? minusViewController else {return}
+        
+        guard let firstNum = firstTextfield.text else {return}
+        
+        
+        guard let secondNum = secondTextfield.text else {return}
+        
+        // String 타입으로 값이 넘어오기 때문에 형변환 필요
+        // But, Int값이 들어오지 않으면 에러 발생! (신중히 사용)
+        let result = Int(firstNum)! - Int(secondNum)!
+        
+        minusViewController.resultString = String(result)
+        
+        minusViewController.modalPresentationStyle = .fullScreen
+        
+        present(minusViewController, animated: true)
+        
+    }
+    
+    
+    // =============== 덧셈 연산자 버튼 ===============
     @IBAction func plusButtonDidTapped(_ sender: Any) {
         
     // UIStoryboard Main에 있는 SecondViewController에 접근
         guard let plusViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "plusViewController") as? plusViewController else {return}
+        
+
         
     // ===== 덧셈을 해서 다음 View에 넘기는 로직 =====
         guard let firstNum = firstTextfield.text else {return}
---
+++ Weekly-project/Week3-project/Week3-project/minusViewController.swift
@@ -0,0 +1,22 @@
+//
+//  minusViewController.swift
+//  Week3-project
+//
+//  Created by yeonsu on 2022/10/07.
+//
+
+import UIKit
+
+class minusViewController: UIViewController {
+
+    var resultString = ""   // 덧셈 결과
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    override func viewDidLoad() {   //viewWillappear도 가능
+        super.viewDidLoad()
+        resultLabel.text = resultString
+    }
+  
+}
