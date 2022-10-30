2254705077b6d616022c1a9737ca31e365919d77
yeonsu <drawingbonny@gmail.com>
2022-10-30 23:31:23 +0900

    feat: Add prepareForReuse method

--- Week4/Week4/kakaoTalkTableViewCell.swift
+++ Week4/Week4/kakaoTalkTableViewCell.swift
@@ -16,14 +16,23 @@ class kakaoTalkTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImageView: UIImageView!
     
+ 
 
     override func awakeFromNib() {
         super.awakeFromNib()
         profileImageView.layer.cornerRadius = 17.5
-        messageCountBgView.layer.cornerRadius = 12
+        messageCountBgView.layer.cornerRadius = 8
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    // prepareForReuse(): 재사용되는 cell의 속성 초기화
+    // 따로 호출하지 않아도 cell이 재사용될 때 자동으로 background 설정 가능
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        backgroundColor = .white
     }
 }
--- Week4/Week4/tableViewController.swift
+++ Week4/Week4/tableViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class tableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     // UITableViewDataSource 프로토콜 채택 시 필수 구현!
-    // 셀의 개수 지정
+    // numberOfRowsInSection: 셀의 개수 지정
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return chattingRoomData.count
     }
     
     // UITableViewDataSource 프로토콜 채택 시 필수 구현!
-    // 테이블 View에 넣을 Cell return
+    // cellForRowAt: 테이블 View에 넣을 Cell data
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Cell에 접근해서 데이터 변경
@@ -56,7 +56,7 @@ class tableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         kakaoTalkTableView.dataSource = self
     }
     
-    let chattingRoomData: [ChattingRoomDataModel] = [ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61")]
+    let chattingRoomData: [ChattingRoomDataModel] = [ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"),ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"),ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61"), ChattingRoomDataModel(profileImage: UIImage(named: "swiftIcon"), name: "iOS 단톡방", lastMessage: "사진을 보냈습니다.", memberCount: "200", time: "오전 1:05", messageCount: "61")]
 }
 
 // Cell에 들어가는 데이터 모델을 구조체로 정의
