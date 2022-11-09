4fec278b5423ce14b1db9684ebd8e911bfc6c2fe
yeonsu <drawingbonny@gmail.com>
2022-11-09 23:54:44 +0900

    first commit

---
+++ Weekly-project/Week7-project/Week7-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week7-project
+//
+//  Created by yeonsu on 2022/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
