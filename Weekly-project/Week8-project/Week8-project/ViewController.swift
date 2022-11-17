fc16d91d72279860182ba9eb75a067d6c938b834
yeonsu <drawingbonny@gmail.com>
2022-11-17 19:33:32 +0900

    feat: Add UIView animation

--- Weekly-project/Week8-project/Week8-project/ViewController.swift
+++ Weekly-project/Week8-project/Week8-project/ViewController.swift
@@ -1,26 +1,40 @@
 //
 //  ViewController.swift
-//  Week2
+//  Week8
 //
-//  Created by yeonsu on 2022/09/26.
+//  Created by yeonsu on 2022/11/19.
 //
 
 import UIKit 
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var newItemView: UIView!
+    @IBOutlet weak var shapeView: UIView!
+    
+    @IBOutlet weak var viewHeight: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
     }
-
-
-    @IBOutlet weak var newItemView: UIView!
     
+
     @IBAction func profileButtonDidtapped(_ sender: Any) {
         print("버튼이 클릭됐습니다!")
         newItemView.backgroundColor = UIColor.lightGray
     }
     
-       }
+    
+    
+    @IBAction func ButtonDidTap(_ sender: Any) {
+        UIView.animate(withDuration: 1, animations: {
+            self.viewHeight.constant = 200
+            self.view.layoutIfNeeded()  // 변경사항에 대한 업데이트 반영
+            // self.view.setNeedsLayout()
+        })
+    }
+    
+}
 
